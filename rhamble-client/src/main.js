a683d17cb2e97908c42d6dc6cd8b25b9c261d4fd
Wyatt Sillavan <wyattrayder@gmail.com>
2024-07-13 13:45:39 -0500

    Firebase implementation

--- rhamble-client/src/main.js
+++ rhamble-client/src/main.js
@@ -6,6 +6,28 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC53G7yjiTCeuwbbYk4BbAJts8U6rQx6jA",
+  authDomain: "rhamble-7a219.firebaseapp.com",
+  projectId: "rhamble-7a219",
+  storageBucket: "rhamble-7a219.appspot.com",
+  messagingSenderId: "791782214563",
+  appId: "1:791782214563:web:db29b902b61429aecb2816",
+  measurementId: "G-D5687STF7Q"
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+const analytics = getAnalytics(firebase);
+
 const app = createApp(App)
 
 app.use(createPinia())
